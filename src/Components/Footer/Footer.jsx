7c998c6d29e8087b7fdfd3c918cd485e56c04a3e
codycubes <boitshepomashamaite777@gmail.com>
2024-07-20 15:58:12 +0200

    removed unnecessary imports

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import './Footer.css';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFacebookF, faLinkedinIn, faPinterestP } from "@fortawesome/free-brands-svg-icons";
 
 export const Footer = () => {
     return (
